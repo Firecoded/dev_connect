032e14c080d64152b621ca07d8fa1c5db4c0e679
firecoded <yepyepguru@gmail.com>
2021-05-19 19:38:45 -0700

    routes setup, users model setup

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const connectDB = require("./config/db");
 
 const app = express();
 
+// connect mongodb
 connectDB();
 
 app.get("/", (req, res) => res.send("Hello"));
+
+// routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
